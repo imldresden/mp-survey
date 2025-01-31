8a191ba205b720d19d5acc558b3f2761b5a174ef
Julián Méndez <menoc.sk27@gmail.com>
2025-01-31 12:42:31 +0100

    modify readme and dummy survey values

--- src/data/create-jsons.py
+++ src/data/create-jsons.py
@@ -6,9 +6,9 @@ import logging
 # properties without a filter or view
 excludeProp = ["Name", "Authors", "Year", "DOI", "Bibtex"]
 
-optionals = ["Secondary Contribution"]
+optionals = ["Optional Category"]
 
-# properties that will be shown in the website
+# categories that will be shown in the website
 includeProp = {
   # fixed
   "Name":"String",
@@ -18,152 +18,27 @@ includeProp = {
   "Year": "Timeline",
   
   # custom categories
-  "2D Devices": "MultiSelect",
-  "MR Devices": "MultiSelect",
-  "Configuration": "MultiSelect",
-  "Temporal": "MultiSelect",
-  "Relationship": "MultiSelect",
-  "Range": "MultiSelect",
-  "Device Dependency": "MultiSelect",
-  "Space": "MultiSelect",
-  "Interaction Dynamics": "MultiSelect",
-  "Anchoring": "MultiSelect",
-
-  "Use Case": "MultiSelect", 
-  "Main Contribution": "MultiSelect",
-  "Secondary Contribution": "MultiSelect",
-  "Evaluation": "MultiSelect",
-
-  "Terminology": "MultiSelect",
-  "Edge Case": "MultiSelect",
+  "CategoryOne": "MultiSelect",
+  "CategoryTwo": "MultiSelect",
+  "CategoryThree": "MultiSelect",
 }
 
 # properties that will be read from the csv, indexed to their supergroups
 categories = {
-  "AR OST HWD": "MR Devices",
-  "AR VST HWD": "MR Devices",
-  "Handheld AR (Smartphone)": "MR Devices",
-  "Handheld AR (Tablet)": "MR Devices",
-  "Handheld AR": "MR Devices",
-  "Spatial AR (projector based)": "MR Devices",
-  "Stereoscopic Projection": "MR Devices",
-  "CAVE": "MR Devices",
-  "VR HWD": "MR Devices",
-
-  "Smartwatch": "2D Devices",
-  "Smartphone": "2D Devices",
-  "Tablet": "2D Devices",
-  "Laptop": "2D Devices",
-  "Desktop": "2D Devices",
-  "Projection": "2D Devices",
-  "Large Display": "2D Devices",
-  "Tabletop": "2D Devices",
-
-  "Symmetric": "Configuration",
-  "Asymmetric": "Configuration",
-  "Remote Control": "Configuration",
-  "Dynamic Lens": "Configuration",
-  "Augmented Display": "Configuration",
-  "VESAD": "Configuration",
-  "Logical Distribution": "Configuration",
-  "Migratory Interface": "Configuration",
-
-  "Parallel": "Temporal", 
-  "Serial": "Temporal", 
-  "Exclusive": "Temporal", 
-          
-  "Single User": "Relationship",
-  "Multi-user - Indvidual Component": "Relationship",
-  "Multi-user - Shared Component": "Relationship",
-          
-  "Near": "Range", 
-  "Personal": "Range", 
-  "Social": "Range", 
-  "Public": "Range",
+  "PropertyC1One": "CategoryOne",
+  "PropertyC1Two": "CategoryOne",
   
-  "Flexible": "Device Dependency", 
-  "Semi-Fixed": "Device Dependency", 
-  "Fixed": "Device Dependency",
+  "PropertyC2One": "CategoryTwo", 
+  "PropertyC2Two": "CategoryTwo", 
           
-  "Co-Located": "Space", 
-  "Remote": "Space",
-          
-  "Unidirectional (2D-centric)": "Interaction Dynamics",
-  "Unidirectional (MR-centric)": "Interaction Dynamics",
-  "Bidirectional": "Interaction Dynamics",
-
-  "Component-coupled": "Anchoring", 
-  "Free": "Anchoring", 
-  "Dynamic": "Anchoring",
-
-  "Term: Hybrid UI": "Terminology",
-  "Term: Hybrid <other>": "Terminology",
-  "Term: Cross-Device": "Terminology",
-  "Term: Cross-Reality": "Terminology",
-  "Term: Multi-Device": "Terminology",
-  "Term: Augmented Display": "Terminology",
-  "Term: Transitional": "Terminology",
-  "Undefined": "Terminology",
-  "Other Terms": "Terminology",
-
-  "Empirical": "Main Contribution",
-  "Artifact": "Main Contribution",
-  "Method": "Main Contribution",
-  "Theory": "Main Contribution",
-  "Dataset": "Main Contribution",
-  "Survey": "Main Contribution",
-  "Opinion": "Main Contribution",
-
-  "Empirical (-)": "Secondary Contribution",
-  "Artifact (-)": "Secondary Contribution",
-  "Method (-)": "Secondary Contribution",
-  "Theory (-)": "Secondary Contribution",
-  "Dataset (-)": "Secondary Contribution",
-  "Survey (-)": "Secondary Contribution",
-  "Opinion (-)": "Secondary Contribution",
-
-  "Informative": "Evaluation",
-  "Demonstration": "Evaluation",
-  "Technical Evaluation": "Evaluation",
-  "Usage": "Evaluation",
-  "Heuristic": "Evaluation",
-  "No Evaluation": "Evaluation", 
-
-  "Development/Authoring": "Use Case", 
-  "Gaming": "Use Case", 
-  "DataVis/Data Analysis": "Use Case", 
-  "SciVis": "Use Case", 
-  "Medical": "Use Case", 
-  "Productivity": "Use Case", 
-  "Collaboration": "Use Case", 
-  "Entertainment": "Use Case", 
-  "3D Object Manipulation": "Use Case", 
-  "3D Design/Sketching": "Use Case", 
-  "Text Entry / Annotations": "Use Case", 
-  "Study": "Use Case", 
-  "Other / None": "Use Case"
+  "PropertyC3One": "CategoryThree",
+  "PropertyC4Two": "CategoryThree",
 }
 
 groups = { 
-  "2D Devices": "Technology",
-  "MR Devices": "Technology",
-
-  "Configuration": "Taxonomy",
-  "Temporal": "Taxonomy",
-  "Relationship": "Taxonomy",
-  "Range": "Taxonomy",
-  "Device Dependency": "Taxonomy",
-  "Space": "Taxonomy",
-  "Interaction Dynamics": "Taxonomy",
-  "Anchoring": "Taxonomy",
-
-  "Main Contribution": "Theory and Contribution", 
-  "Secondary Contribution": "Theory and Contribution", 
-  "Use Case": "Theory and Contribution", 
-  "Evaluation": "Theory and Contribution", 
-
-  "Terminology": "Meta", 
-  "Edge Case": "Meta"
+  "CategoryOne": "GroupOne",
+  "CategoryTwo": "GroupOne",
+  "CategoryThree": "GroupTwo"
 }
 
 def get_arguments():
