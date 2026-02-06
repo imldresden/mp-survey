e329ff111b43bc6304af2137f46f23635560ddae
Julián Méndez <menoc.sk27@gmail.com>
2026-02-06 11:06:39 +0100

    deploy

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig /*, splitVendorChunkPlugin*/ } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { viteSingleFile } from "vite-plugin-singlefile"
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [svelte(), viteSingleFile() /*, splitVendorChunkPlugin()*/],
-});
